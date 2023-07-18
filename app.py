1628d95c3c176c354ea619cc530d5d857e83a0bc
AlphaIdylSaythTG <123337877+AlphaIdylSaythTG@users.noreply.github.com>
2023-07-18 12:43:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import openai
+
 
 def main():
     st.title("Video Uploader and Option Menu")
@@ -21,24 +23,28 @@ def main():
     )
 
 
+    with open("api-key.txt", "r") as file:
+        openai.api_key = file.read().strip()
+
+
 
     # Show content based on the selected option
     if selected_page == "Summary":
         st.write("This is the Summary page.")
-        problem  = "airbag failure" #we will get this from the user (react front end)
-        prompt = ""
-        #prompt = "give me a fishbone analysis for airbag failure "
+        with open("sample.txt", "r") as f:
+            text_data = f.read()
+
+        prompt = "give me a summary of this: " + text_data
         # Make the API call
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}]
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}]
 
         )
 
-
-generated_text = response.choices[0].message.content
-
-print(generated_text)
+        generated_text = response.choices[0].message.content
+        print(generated_text)
+        st.write(generated_text )
 
 
     elif selected_page == "Chat With Meeting":
